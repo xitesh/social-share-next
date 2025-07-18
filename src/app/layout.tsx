cec7a2df16eddc58b28c9ef67d6fc3488b9cf0b8
Nitesh Nagar <itsniteshnagar@gmail.com>
2025-07-18 11:16:24 +0530

    feat: add responsive navigation with desktop and mobile support
    - Implemented DesktopNavbar and MobileNavbar components for responsive navigation.
    - Added ModeToggle for theme switching.
    - Integrated Navbar into the main layout.
    - Updated layout to include a sidebar and main content area.
    - Enhanced styling with Tailwind CSS and Radix UI components.
    - Introduced utility functions for class name management.
    - Updated global styles to support new theme variables and animations.
    - Added new dependencies for improved UI components and animations.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,13 +27,35 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="min-h-screen">
+              <Navbar />  
+              <main className="py-8">
+                {/* containr to center the content */}
+                <div className="max-w-7xl mx-auto px-4">
+                  <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                    <div className="hidden lg:block lg:col-span-3">
+                      Sidebar
+                    </div>
+                    <div className="lg:col-span-9">
+                      {children}
+                    </div>
+                  </div>
+                </div>
+              </main>
+            </div>
+          </ThemeProvider>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
+import ModeToggle from '@/components/ModeToggle'
+import { Button } from '@/components/ui/button'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+    <div className='m-4'>
+      <h1>Home Page Content.</h1>
     </div>
   )
 }
